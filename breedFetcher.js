a60a8ee3464dda4888cd0da1692730525d481741
leam <leam.murphy1@gmail.com>
2021-06-04 00:54:58 +0000

    Added test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,30 @@
-const myArgs = process.argv.slice(2);
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + myArgs[0], (error, response, body) => {
-  if (error) {
-    console.log("Request Failed!:", error);
-  }
-  if (response && response.statusCode === 200) {
-    if (body === "[]") {
-      console.log(`no cats called ${myArgs[0]} found`);
-    } else {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      // console.log("Request Failed!:", error);
+      callback(error, null);
     }
+    if (response && response.statusCode === 200) {
+      if (body === "[]") {
+        // console.log(`no cats called ${breedName} found`);
+        callback(`no cats called ${breedName} found`, null);
+      } else {
+        const data = JSON.parse(body);
+        // console.log(data[0].description);
+        callback(null ,data[0].description);
+      }
     
-  }
+    }
 
-});
+  });
 
+};
+
+
+module.exports = { fetchBreedDescription };
 
 // | grep "status"
 // console.log('error', error);
